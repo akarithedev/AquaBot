cef26275b0bf0c3b266087c76d137f0d17c22395
MariaTheDev <62624271+mariathedevofficial@users.noreply.github.com>
2022-03-13 17:33:53 +0200

    push

--- AquaClient.js
+++ AquaClient.js
@@ -3,21 +3,20 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.write("I'm alive");
     res.end();
-}).listen(8080);
+}).listen();
 
 const discord = require("discord.js");
-const Intents = discord.Intents;
 const bot = new discord.Client({ intents: 32767, ws: { properties: { $browser: "Discord Android" } }});
 const fs = require("fs");
-const { token, prefix, owner } = require("./config.json");
+const { prefix, owner } = require("./config.json");
 const Util = require("./AquaUtil.js");
 const { Manager } = require("erela.js");
 const { nodes } = require("./config.json");
-const embed = new discord.MessageEmbed()
 const { Database } = require("quickmongo");
 const { config } = require("dotenv");
 const Spotify = require("erela.js-spotify");
 const Apple = require("erela.js-apple");
+const Deezer = require("erela.js-deezer");
 const clientID = "ea41a85d7f444e25b83942adb2dfba70";
 const clientSecret = "1b8016b912d64aee80b1980ae0ada45c";
 require("./structures/lavaplayer");
@@ -29,7 +28,7 @@ bot.prefix = prefix;
 bot.devs = owner;
 bot.utils = new Util(bot);
 bot.snek = require("axios");
-bot.database = new Database(`mongodb+srv://aquadb:aquadb557@aqua1.3zpil.mongodb.net/AquaBotretryWrites=true&w=majority`);
+bot.database = new Database(`mongodb://admin:${encodeURIComponent("aP@55word")}@ca1.node.sneakyhub.com:30037/aqua?authSource=admin`);
 bot.music = new Manager({
     nodes,
     defaultSearchPlatform: 'youtube music',
@@ -43,12 +42,14 @@ bot.music = new Manager({
       clientID,
       clientSecret
     }),
-    new Apple()
+    new Apple(),
+    new Deezer()
   ]
 })
 var text = fs.readFileSync('./emojis.json')
 var obj = JSON.parse(text)
 bot.emoji = obj;
+bot.events = new discord.Collection();
 
 bot.embed = new discord.MessageEmbed()
 config({
@@ -56,8 +57,8 @@ config({
 });
 
 bot.on("ready", async () => {
-    bot.user.setActivity(`Aqua Network`, { type: "WATCHING" })
-    console.log("The bot is now online")
+    bot.user.setActivity(`Advanced Bot`, { type: "WATCHING" })
+    console.log(`Connected as ${bot.user.tag}`)
     bot.music.init(bot.user.id)
     bot.database.connect()
 })
@@ -70,7 +71,7 @@ bot.database.on("error", () => {
 })
 bot.on("raw", (d) => bot.music.updateVoiceState(d));
 
-["command"].forEach(handler => {
+["command", "event"].forEach(handler => {
     require(`./handlers/${handler}`)(bot);
 });
 
@@ -89,19 +90,21 @@ bot.on("messageCreate", async (message) => {
     let command = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
 
     if (!command) {
-        embed.setTitle("Command Error")
-        embed.setDescription(`${bot.emoji.error} This command does not exist`)
-        embed.setColor("RED")
+        const embed = new discord.MessageEmbed()
+        .setTitle("Command Error")
+        .setDescription(`${bot.emoji.error} This command does not exist`)
+        .setColor("RED")
         console.log(`User ${message.author.tag} tried to use a non existent command (${command})`)
         return message.reply({ embeds: [embed] })
     }
     if (command.ownerOnly) {
         if (!bot.devs.includes(message.author.id)) {
-            embed.setTitle("Command Error")
-            embed.setDescription(`${bot.emoji.error} You do not have permission to use this command.`)
-            embed.setColor("RED")
+            const embed2 = new discord.MessageEmbed()
+            .setTitle("Command Error")
+            .setDescription(`${bot.emoji.error} You do not have permission to use this command.`)
+            .setColor("RED")
             console.log(`User ${message.author.tag} tried to use a restricted command (${command})`)
-            return message.reply({ embeds: [embed] })
+            return message.reply({ embeds: [embed2] })
         }
     }
     if (command) command.run(bot, message, args)
@@ -121,12 +124,15 @@ bot.music.on("nodeError", (node, error) => {
 
 bot.music.on("trackStart", async(player, track) => {
     let channel = bot.channels.cache.get(player.textChannel);
-    embed.setAuthor("Started Playing", "https://imgs.search.brave.com/J2j5NufZNBJIdmzIn3ZIc1_D47PhO7-nAStKMddnK9w/rs:fit:400:300:1/g:ce/aHR0cHM6Ly9tZWRp/YS5naXBoeS5jb20v/bWVkaWEvSHdqelRU/b2dLWUdJTS9naXBo/eS5naWY.gif")
-    embed.setThumbnail(track.thumbnail)
-    embed.setDescription(`**[${track.title}](${track.uri})**\n**Volume**: \`${player.volume}%\`\n**Filter**: \`none\``)
-    embed.setFooter(`Requester: ${track.requester.tag}`, track.requester.displayAvatarURL({ size: 4096, format: "png", dynamic: true }))
-    embed.setColor("BLUE")
-    let msg = await channel.send({ embeds: [embed] })
+    let filters = await bot.database.get(`filters_${player.guild}`);
+
+    const embed3 = new discord.MessageEmbed()
+    .setAuthor("Started Playing", "https://imgs.search.brave.com/oyv9NvW9g1RvUWiERhs3gpdQ4vo-tiqoXppHuAkDBIU/rs:fit:700:700:1/g:ce/aHR0cHM6Ly9tZWRp/YTAuZ2lwaHkuY29t/L21lZGlhL1NTOHph/TkZJYnd5amNQZGg4/VS9zb3VyY2UuZ2lm.gif")
+    .setThumbnail(track.thumbnail)
+    .setDescription(`**[${track.title}](${track.uri})**\n**Volume**: \`${player.volume}%\`\n**Filter(s)**: \`${filters.length !== 0 ? filters.join(', ') : 'None'}\``)
+    .setFooter(`Requester: ${track.requester.tag}`, track.requester.displayAvatarURL({ size: 4096, format: "png", dynamic: true }))
+    .setColor("BLUE")
+    let msg = await channel.send({ embeds: [embed3] })
     if(track.isStream) return;
     setTimeout(() => {
         if(track.isSeekable) {
@@ -138,11 +144,11 @@ bot.music.on("trackStart", async(player, track) => {
 bot.music.on("queueEnd", (player) => {
     player.destroy()
     let channel = bot.channels.cache.get(player.textChannel);
-    let embed2 = new discord.MessageEmbed()
+    let embed4 = new discord.MessageEmbed()
     .setTitle("Queue ended")
     .setDescription(`The music queue has ended.`)
     .setColor("BLUE")
-    channel.send({ embeds: [embed2] })
+    channel.send({ embeds: [embed4] })
 });
 
 bot.music.on("trackEnd", async(player) => {
