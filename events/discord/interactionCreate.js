13c1f396a5b3bc80e141011eb5839efea656a215
MariaTheDev <62624271+mariathedevofficial@users.noreply.github.com>
2022-03-18 19:28:36 +0200

    push

--- events/discord/interactionCreate.js
+++ events/discord/interactionCreate.js
@@ -1,9 +1,27 @@
 const discord = require("discord.js")
-
 module.exports.run = async(bot, interaction) => {
 	if(!interaction.isCommand()) return;
     
 	let command = bot.slashCommands.get(interaction.commandName);
 	if(!command) return;
+	if(command.ownerOnly) {
+		if (!bot.devs.includes(interaction.user.id)) {
+            const embed1 = new discord.MessageEmbed()
+            .setTitle("Command Error")
+            .setDescription(`${bot.emoji.error} You do not have permission to use this command.`)
+            .setColor("RED")
+            console.log(`User ${message.author.tag} tried to use a restricted command (${cmd})`)
+            return interaction.reply({ embeds: [embed1] })
+        }
+	}
+	if(command.nsfwOnly) {
+		if(!interaction.channel.nsfw) {
+				const embed2 = new discord.MessageEmbed()
+				.setTitle("Command Error")
+				.setDescription(`${bot.emoji.error} This command can be used only in nsfw channels`)
+				.setColor("RED")
+				return interaction.reply({ embeds: [embed2] })
+		}
+	  }
 	command.run(bot, interaction, interaction.options);
 }
