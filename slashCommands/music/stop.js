97f8d363c4ae093ba6e090cc585dbb59dfb84141
MariaTheDev <62624271+mariathedevofficial@users.noreply.github.com>
2022-03-19 19:47:37 +0200

    push

--- slashCommands/music/stop.js
+++ slashCommands/music/stop.js
@@ -18,21 +18,24 @@ module.exports = {
             embed.setDescription(`${bot.emoji.error} You need to be in a voice channel to use the stop command.`)
             embed.setColor("BLUE")
             embed.setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL({format: "png", dynamic: true, size: 2048}))
-            return interaction.reply({embeds: [embed]})
+            interaction.reply({embeds: [embed]})
+            return;
         }
 
         if(!player) {
             embed.setDescription(`${bot.emoji.error} There is no song/s playing within this guild.`)
             embed.setColor("BLUE")
             embed.setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL({format: "png", dynamic: true, size: 2048}))
-            return interaction.reply({embeds: [embed]})
+            interaction.reply({embeds: [embed]})
+            return;
         }
          
         if(player.voiceChannel !== voiceChannel.id) {
             embed.setDescription(`${bot.emoji.error} I'm being used in another voice channel.`)
             embed.setColor("BLUE")
             embed.setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL({format: "png", dynamic: true, size: 2048}))
-            return interaction.reply({embeds: [embed]})
+             interaction.reply({embeds: [embed]})
+             return;
         }
 
 
@@ -40,6 +43,7 @@ module.exports = {
         embed.setDescription(`${bot.emoji.success} Successfully stopped the music client.`)
         embed.setColor("BLUE")
         embed.setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL({format: "png", dynamic: true, size: 2048}))
-        return interaction.reply({embeds: [embed]});
+        interaction.reply({embeds: [embed]})
+        return;
     }
 }
